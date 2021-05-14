a2c0913b3395e4002887381c48a10cb6dd855e1f
zzq-s <54193504+zzq-s@users.noreply.github.com>
2021-05-14 10:54:21 +0900

    Delete 1.Two_Sum directory

--- 1.Two_Sum/solution.py
+++
@@ -1,9 +0,0 @@
-#https://leetcode.com/problems/two-sum/
-
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashtable = dict()
-        for i,num in enumerate(nums):
-            if target - num in hashtable:
-                return [hashtable[target - num],i]
-            hashtable[num] = i
