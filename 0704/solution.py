f4faa1097de1c1d763a026207997dbf4e2866d67
zzq-s <zzq13237356748@gmail.com>
2021-08-30 15:06:16 +0900

    add 0704

---
+++ 0704/solution.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/binary-search/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            pivot = left + (right - left) // 2
+            if nums[pivot] == target:
+                return pivot
+            elif target < nums[pivot]:
+                right = pivot - 1
+            else:
+                left = pivot + 1
+        return -1
